fbf81bd1152ce1779a4b5168f0abc6be1fc73733
Marco Costa <marcocosta1618@gmail.com>
2023-06-09 14:55:25 +0200

    order import statements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
 import { WorldAtlas } from "./components/WorldAtlas";
+import { Footer } from "./components/Footer";
+
 
 const dim = {
   w: 640,
